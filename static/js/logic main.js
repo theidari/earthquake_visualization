6b3ee2bd6754810227da680ef9a6736f5d0a6ed9
Ali Taghipour Heidari <theidari.ali@gmail.com>
2023-02-14 15:59:41 -0500

    create about section

---
+++ static/js/logic main.js
@@ -0,0 +1,9 @@
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function.
+    createFeatures(data.features);
+  });
+  
